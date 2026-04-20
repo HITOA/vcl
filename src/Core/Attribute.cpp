95c4a94d19b031cec17ed829763505959188246c
HITOA <charli.poirier@gmail.com>
2026-04-20 14:50:03 +0200

    fix

--- src/Core/Attribute.cpp
+++ src/Core/Attribute.cpp
@@ -5,4 +5,6 @@
 void VCL::AttributeTable::AddDefaults(IdentifierTable& table) {
     AddDefinition(table.Get("EntryPoint"), 0, 0);
     AddDefinition(table.Get("NoMangle"), 0, 0);
+    AddDefinition(table.Get("StrictIEEE"), 0, 0);
+    AddDefinition(table.Get("AllowApproxFunctions"), 0, 0);
 }
